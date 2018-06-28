9b5ae1742389daf7f595acb18831d970a886402b
SamGomes <sam.s.gomes@sapo.pt>
2018-06-28 03:17:33 +0100

    Some last updates of the day...

--- Assets/Scripts/EndScreenFunctionalities.cs
+++ Assets/Scripts/EndScreenFunctionalities.cs
@@ -9,6 +9,11 @@ public class EndScreenFunctionalities : MonoBehaviour
     public Button UIRestartGameButton;
     public GameObject UIAlbumCollectionDisplay;
 
+    public GameObject UIVictoryOverlay;
+    public GameObject UILossOverlay;
+
+
+
     private void RestartGame()
     {
         GameSceneManager.LoadStartScene();
@@ -21,9 +26,36 @@ public class EndScreenFunctionalities : MonoBehaviour
         }
     }
 
+    private IEnumerator HideAfterDelay(GameObject obj, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        obj.SetActive(false);
+    }
+
+
     // Use this for initialization
     void Start()
     {
+        UIVictoryOverlay.SetActive(false);
+        UILossOverlay.SetActive(false);
+
+        if (GameGlobals.currGameState == GameProperties.GameState.VICTORY)
+        {
+            UIVictoryOverlay.SetActive(true);
+            StartCoroutine(HideAfterDelay(UIVictoryOverlay, 5.0f));
+        }
+        else if(GameGlobals.currGameState == GameProperties.GameState.LOSS)
+        {
+            UILossOverlay.SetActive(true);
+            StartCoroutine(HideAfterDelay(UILossOverlay, 5.0f));
+
+        }
+        else
+        {
+            Debug.Log("[ERROR]: Game state returned NON FINISHED on game end!");
+            return;
+        }
+
         if (!GameProperties.isSimulation || (GameProperties.isSimulation && GameProperties.numGamesToSimulate > 0))
         {
             foreach (Album album in GameGlobals.albums)
